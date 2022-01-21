3b4c2b082c1b9d04dc61b0eed202683e3e0eb36d
Juan Manuel Camara Diaz (Teams) <JuanManuel.Camara@e-campus.uab.cat>
2022-01-21 23:01:13 +0100

    Mouse

--- js/p5.js
+++ js/p5.js
@@ -1,15 +1,19 @@
 const Boxes = [];
 
 class Box {
-    constructor (x, y) {
+    constructor(x, y) {
         this.x = x;
         this.y = y;
-        this.x_angle = 0;
-        this.y_angle = 0;
+        this.x_angle = random(0, 360);
+        this.y_angle = random(0, 360);
         this.rotateXSpeed = random(-0.01, 0.01);
         this.rotateYSpeed = random(-0.01, 0.01);
-        this.size = random(10, 50);
+        this.size = random(20, 50);
     }
+}
+
+function change() {
+    resizeCanvas(windowWidth, windowHeight);
 }
 
 function setup() {
@@ -26,23 +30,31 @@ function step_box(box_obj) {
     box_obj.y_angle += box_obj.rotateYSpeed;
 }
 
-function draw_box(box_obj) {
+function draw_box(box_obj, max_distance) {
+    let distance = Math.hypot(box_obj.x + ((windowWidth / 2) - mouseX), box_obj.y + ((windowHeight / 2) - mouseY));
+    let size = 5;
+    if (distance < 200) {
+        size = (255 - distance) / 20;
+    }
     push();
-    stroke(5);
+    stroke(9);
     translate(box_obj.x, box_obj.y, 0);
     rotateY(box_obj.x_angle);
     rotateX(box_obj.y_angle);
-    box(box_obj.size);
+    box( box_obj.size + size);
     pop();
 }
 
 function draw() {
     background(20, 20, 20);
+    let max_distance = Math.hypot(width / 2, height / 2);
     for (var i = 0; i < Boxes.length; i++) {
-        draw_box(Boxes[i]);
+        draw_box(Boxes[i], max_distance);
     }
 
     for (var i = 0; i < Boxes.length; i++) {
         step_box(Boxes[i]);
     }
+
+    //console.log(mouseX, mouseY);
 }
